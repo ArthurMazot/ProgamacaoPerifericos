69bc2b84170ac09d109f3d3bf592d8667a3b5807
ArthurMazot <arthurmazot@hotmail.com>
2026-09-06 14:09:36 -0300

    Enhance ESP32 communication with response validation
    Refactor ESP32 code to include response handling and CRC calculation.

--- T1/esp32.c
+++ T1/esp32.c
@@ -1,35 +1,66 @@
 #include <Arduino.h>
 
+const char comm[3][6] = {0x01, 0x04, 0x00, 0x01, 0x00, 0x01,  // Temperatura [0]
+                         0x01, 0x04, 0x00, 0x02, 0x00, 0x01,  // Umidade [1]
+                         0x01, 0x04, 0x00, 0x01, 0x00, 0x02}; // Temperatura e umidade [2]
+
 char msg_tx[8] = {};
+char msg_rx[8] = {};
+
+//==========================================//
 
 void setup(){
     Serial.begin(9600);
     delay(1000);
 }
 
+//==========================================//
+
 void loop(){
     uint8_t resp = 0;
-    while(resp == 0) resp = getResp();
+    while(resp == 0) resp = getResp(); 
 
-    switch(resp){
-        case 0:
-        case 1:
-        case 2:
-        default:;
+    if(resp < 0 && resp > 2){ // verifica se a resposta é válida
+        // colocar alguma coisa aqui
     }
+
+    for(int i = 0; i < 6; i++) // coloca a mensagem de resposta no buffer de envio
+            msg_tx[i] = comm[resp][i];
+        
+    calculateCRC((unsigned char *)msg_tx, 6); // calcula o CRC da mensagem de resposta
+
+    // enviar a mensagem para o sensor
+
 
 
 }
 
-uint8_t getResp(){
+//==========================================//
+
+uint8_t getResp(){ // pega a resposta do front pela serial
     uint8_t dado = 0;
     if(Serial.available()>0)
         dado = Serial.read();
 
     delay(1000);
-    return dado;
+    return dado - '0'; // converte o char recebido para int
 }
 
-void sendResp(){
+//==========================================//
 
+void calculateCRC(unsigned char *frame, int tamanho) { // Função pega do aquivo .c disponibilizado pelo professor
+  unsigned int crc = 0xFFFF;            // Initialize CRC to 0xFFFF
+  for (int n = 0; n < tamanho; n++) {
+    crc ^= frame[n];                    // XOR the frame byte with the CRC
+    for (int m = 0; m < 8; m++) {
+      if (crc & 0x0001) {               // Check if the LSB of the CRC is 1
+        crc >>= 1;                      // Right shift the CRC
+        crc ^= 0xA001;                  // XOR the CRC with the polynomial 0xA001
+      } else {
+        crc >>= 1;                      // Right shift the CRC
+      }
+    }
+  }
+  msg_tx[7] = (crc >> 8) & 0xFF;     // Separando a parte alta do CRC  
+  msg_tx[6] = crc & 0xFF;
 }
